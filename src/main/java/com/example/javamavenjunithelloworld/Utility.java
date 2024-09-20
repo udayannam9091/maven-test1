45db4e06dfb1cead0c060a6a5c07a27791851485
uday <udaychandrareddyannam@gmail.com>
2024-09-20 23:06:55 +0530

    UDAYA0001 - wrote the code for the area of square

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,8 @@
+public class Uday{      
+    public static void main(String args[])  
+    {  
+       int s=13;  
+       int area_square=s*s;  
+       System.out.println("Area of the square="+area_square);  
+    }  
+}  
